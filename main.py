58953756b6bf2e22eda5962f345f84af88157d39
ca1b <calb.bat@proton.me>
2024-04-09 21:20:50 -0500

    turning a python cli into a webapp first time I ever did this by myself

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import os
-import math
-import BMICalc
+from __init__ import create_app
 
-def contact():
-    print("hello!\n")
-    f = float(input("height in feet? (please type a whole number): "))
-    i = float(input("now input the remainder of your height in inches: "))
-    w = float(input("please input your height in pounds: "))
-    h = (f*12.0 + i) * 0.025
-    print("\nnow calculating...")
-    bmiCalc = BMICalc.BMICalc(h,w)
-    print("Your bmi value is " + str(bmiCalc.bmi_value()))
-    print("You are " + bmiCalc.giveBMI())
+app = create_app()
 
-contact()
+if __name__ == '__main__':
+    app.run(debug=True)
